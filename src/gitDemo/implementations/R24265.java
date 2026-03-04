975856ea7307a0af35e60c7dd13eca470c25adb4
patrik <smudpa60@osu.cz>
2026-03-04 12:05:25 +0100

    Pushin

---
+++ src/gitDemo/implementations/R24265.java
@@ -0,0 +1,32 @@
+package gitDemo.implementations;
+
+import gitDemo.interfaces.IC;
+import gitDemo.types.DriverTime;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class R24265 implements IC {
+
+    @Override
+    public List<DriverTime> filterRecordsByDriver(List<DriverTime> driverTimes) {
+
+        Map<String, DriverTime> bestTimes = new HashMap<>();
+
+        for (DriverTime driverTime : driverTimes) {
+            String name = driverTime.getName();
+
+            if (!bestTimes.containsKey(name) || driverTime.getTimeInMs() < bestTimes.get(name).getTimeInMs()) {
+                bestTimes.put(name, driverTime);
+            }
+        }
+        return new ArrayList<>(bestTimes.values());
+    }
+
+    @Override
+    public String getStudyNumber() {
+        return "R24265";
+    }
+}
