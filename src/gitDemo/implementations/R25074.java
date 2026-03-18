129e11f020638de4f7b5c3b18f924108b3d4e63a
Vilém Žamboch <vilemzamboch.official@seznam.cz>
2026-03-18 09:12:37 +0100

    Implementation now should work

--- src/gitDemo/implementations/R25074.java
+++ src/gitDemo/implementations/R25074.java
@@ -7,31 +7,33 @@ import java.util.HashMap;
 import java.util.ArrayList;
 import gitDemo.types.DriverTime;
 
-public class R25074 implements IC{
-    //@Override
-    public List<StringDriverTimeTuple> convert(String text) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+public class R25074 implements IC {
 
+    @Override
     public List<DriverTime> filterRecordsByDriver(List<DriverTime> driverTimes) {
 
-        //toto zatím není funkční implementace
+        Map<String, DriverTime> bestTimes = new HashMap<>();
 
-        List<DriverTime> driverTimeRecords = new ArrayList<>();
+        for (DriverTime dt : driverTimes) {
 
-        for (int i = 0; i < driverTimes.size(); i++) {
+            String name = dt.getName();
 
+            if (!bestTimes.containsKey(name)) {
+                bestTimes.put(name, dt);
+            } else {
+                DriverTime best = bestTimes.get(name);
 
+                if (dt.getTimeInMs() < best.getTimeInMs()) {
+                    bestTimes.put(name, dt);
+                }
+            }
         }
 
-
-        return driverTimes;
+        return new ArrayList<>(bestTimes.values());
     }
-
 
     @Override
     public String getStudyNumber() {
         return "r25074";
     }
-
 }
